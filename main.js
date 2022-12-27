536319ec33abd5a26da45cea95017823ee985324
utgeraguyo <117796403+utgeraguyo@users.noreply.github.com>
2022-12-28 05:35:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-//캔버스 세팅
-
 let canvas;
 let ctx;
 canvas = document.createElement("canvas");
 ctx = canvas.getContext("2d");
-canvas.width=1000;
-canvas.height=600;
+canvas.width=3000;
+canvas.height=1800;
 document.body.appendChild(canvas);
 
 let backgroundImage, buttonImage;
@@ -14,10 +12,7 @@ function loadImage(){
     backgroundImage = new Image();
     backgroundImage.src = "images/ball.png";
 
-    buttonImage = new Image();
-    buttonImage.src = "images/startButton.png";
-
-    for(let i=1; i<=45; i++){
+    for(let i=1; i<=20; i++){
         numImage[i] = new Image();
         numImage[i].src = `images/number${i}.png`;
     }
@@ -46,10 +41,10 @@ function generateRandomValue(min,max){
 let num = new Array();
 let n;
 function createNumber(){
-    num[1] = generateRandomValue(1,45);
+    num[1] = generateRandomValue(1,20);
     let i = 2;
-    while(i<=7){
-        n = generateRandomValue(1,45);
+    while(i<=5){
+        n = generateRandomValue(1,20);
     
         let b = 0;
         for(let j=1; j<=i-1; j++){
@@ -68,15 +63,11 @@ function createNumber(){
 
 function render(){
     ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(buttonImage, 300, 350, 400, 200);
-    if(num[1]!=null) ctx.drawImage(numImage[num[1]], 130, 190, 100, 100);
-    if(num[2]!=null) ctx.drawImage(numImage[num[2]], 237, 190, 100, 100);
-    if(num[3]!=null) ctx.drawImage(numImage[num[3]], 345, 190, 100, 100);
-    if(num[4]!=null) ctx.drawImage(numImage[num[4]], 450, 190, 100, 100);
-    if(num[5]!=null) ctx.drawImage(numImage[num[5]], 555, 190, 100, 100);
-    if(num[6]!=null) ctx.drawImage(numImage[num[6]], 662, 190, 100, 100);
-    if(num[7]!=null) ctx.drawImage(numImage[num[7]], 790, 190, 100, 100);
-    
+    if(num[1]!=null) ctx.drawImage(numImage[num[1]], 670, 570, 300, 300);
+    if(num[2]!=null) ctx.drawImage(numImage[num[2]], 990, 570, 300, 300);
+    if(num[3]!=null) ctx.drawImage(numImage[num[3]], 1310, 570, 300, 300);
+    if(num[4]!=null) ctx.drawImage(numImage[num[4]], 1630, 570, 300, 300);
+    if(num[5]!=null) ctx.drawImage(numImage[num[5]], 1950, 570, 300, 300);    
 }
 
 function main(){
